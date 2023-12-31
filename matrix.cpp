0b265619954008bd06d9866cfa09e69082d3af6a
Pran Somboonprasit <75711480+narnn@users.noreply.github.com>
2023-12-31 18:15:58 +0700

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,100 +1,100 @@
 /*
     Pran Somboonprasit
 */
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
+#include<iostream>
 int matrixA[100][100];
 int matrixB[100][100];
 int result[100][100];
+int i,j,k,l,m = 1;
 void matrixCalculation(int Row,int Col,char options);
 void CalculateAddMatrix(int Row,int Col);
 void CalculateMultiMatrix(int Row,int Col);
 void CalculateSubtractMatrix(int Row,int Col);
 void printMatrix(int Row,int Col,int math[100][100]);
-int i,j,k,l = 1,m = 1;
+using namespace std;
 int main(){
     char menu,options;
-    system("color 2");
-    printf(" Select a function 1.Matrix 1x1 2.Matrix 2x2 3.Matrix 3x3 4.Matrix 4x4\n");
-    printf("\t\t 5.Choose the matrix itself");
-    printf(" : ");
-    scanf("%s",&menu);
+    system("color A");
+    cout << "Select a function 1.Matrix 1x1 2.Matrix 2x2 3.Matrix 3x3 4.Matrix 4x4\n";
+    cout << "\t\t  5.Choose the matrix itself";
+    cout << " : ";
+    cin >> menu;
     if(menu == '1'){
         system("cls");
-        printf(" Select Options(A,B,C)\n");
-        printf(" A.Plus B.Subtract C.Multiply : ");
-        scanf("%s",&options);
+        cout << " Select Options(A,B,C)\n";
+        cout << " A.Plus B.Subtract C.Multiply : ";
+        cin >> options;
         matrixCalculation(1,1,options);
     }
     else if(menu == '2'){
         system("cls");
-        printf(" Select Options(A,B,C)\n");
-        printf(" A.Plus B.Subtract C.Multiply : ");
-        scanf("%s",&options);
+        cout << " Select Options(A,B,C)\n";
+        cout << " A.Plus B.Subtract C.Multiply : ";
+        cin >> options;
         matrixCalculation(2,2,options);
     }
     else if(menu == '3'){
         system("cls");
-        printf(" Select Options(A,B,C)\n");
-        printf(" A.Plus B.Subtract C.Multiply : ");
-        scanf("%s",&options);
+        cout << " Select Options(A,B,C)\n";
+        cout << " A.Plus B.Subtract C.Multiply : ";
+        cin >> options;
         matrixCalculation(3,3,options);
     }
     else if(menu == '4'){
         system("cls");
-        printf(" Select Options(A,B,C)\n");
-        printf(" A.Plus B.Subtract C.Multiply : ");
-        scanf("%s",&options);
+        cout << " Select Options(A,B,C)\n";
+        cout << " A.Plus B.Subtract C.Multiply : ";
+        cin >> options;
         matrixCalculation(4,4,options);
     }
     else if(menu == '5'){
         system("cls");
-        printf(" Choose Row ");
-        scanf("%d",&i);
-        printf(" Choose Colum ");
-        scanf("%d",&j);
-        printf(" Select Options(A,B,C)\n");
-        printf(" A.Plus B.Subtract C.Multiply : ");
-        scanf("%s",&options);
+        cout << " Choose Row ";
+        cin >> i;
+        cout << " Choose Colum ";
+        cin >> j;
+        cout << " Select Options(A,B,C)\n";
+        cout << " A.Plus B.Subtract C.Multiply : ";
+        cin >> options;
         matrixCalculation(i,j,options);
     }
+    return 0;
 }
 void matrixCalculation(int Row,int Col,char options){
     system("cls");
-    printf("\tMatrix A\n");
+    cout << "\tMatrix A\n";
     for(i=0;i<Row;i++){
         for(j=0;j<Col;j++){
-            printf(" %d.Enter Numbers [%d][%d] : ",l++,i,j);
-            scanf("%d",&matrixA[i][j]);
+            cout << " " << l++ << ".Enter Numbers [" << i << "][" << j << "] : ";
+            cin >> matrixA[i][j];
         }
     }
     for(i=0;i<Row;i++){
         for(j=0;j<Col;j++){
-            printf("\t[%d]",matrixA[i][j]);
+            cout << "\t[" << matrixA[i][j] << "]";
         }
-    printf("\n");
+        cout << "\n";
     }
-    printf("\n\tMatrix B\n");
+    cout << "\n\tMatrix B\n";
     for(i=0;i<Row;i++){
         for(j=0;j<Col;j++){
-            printf(" %d.Enter numbers [%d][%d] : ",m++,i,j);
-            scanf("%d",&matrixB[i][j]);
+            cout << " " << m++ << ".Enter numbers [" << i << "][" << j << "] : ";
+            cin >> matrixB[i][j];
         }
     }
     for(i=0;i<Row;i++){
         for(j=0;j<Col;j++){
-            printf("\t[%d]",matrixB[i][j]);
+            cout << "\t[" << matrixB[i][j] << "]";
         }
-    printf("\n");
+        cout << "\n";
     }
-    if(options == 'a'||options == 'A'){
+    if(options == 'a' || options == 'A'){
         CalculateAddMatrix(Row,Col);
     }
-    else if(options == 'b'||options == 'B'){
+    else if(options == 'b' || options == 'B'){
         CalculateSubtractMatrix(Row,Col);
     }
-    else if(options == 'c'||options == 'C'){
+    else if(options == 'c' || options == 'C'){
         CalculateMultiMatrix(Row,Col);
     }
 }
@@ -104,16 +104,16 @@ void CalculateAddMatrix(int Row,int Col){
             result[i][j] = matrixA[i][j] + matrixB[i][j];
         }
     }
-    printf("\n\tMatrix Plus\n");
+    cout << "\n\tMatrix Plus\n";
     printMatrix(Row,Col,result);
 }
-void CalculateSubtractMatrix(int Row,int Col){
+void CalculateSubtractMatrix(int Row,int Col) {
     for(i=0;i<Row;i++){
         for(j=0;j<Col;j++){
             result[i][j] = matrixA[i][j] - matrixB[i][j];
         }
     }
-    printf("\n\tMatrix Subtract\n");
+    cout << "\n\tMatrix Subtract\n";
     printMatrix(Row,Col,result);
 }
 void CalculateMultiMatrix(int Row,int Col){
@@ -123,21 +123,26 @@ void CalculateMultiMatrix(int Row,int Col){
         for(j=0;j<Col;j++){
             int sum = 0;
             for(k=0;k<splitMatrix;k++){
-                result[i][j]+=(matrixA[i][k] * matrixB[k][j]);
+                result[i][j] += (matrixA[i][k] * matrixB[k][j]);
             }
         }
     }
-    printf("\n\tMatrix Multiply\n");
+    cout << "\n\tMatrix Multiply\n";
     printMatrix(Row,Col,result);
 }
-void printMatrix(int Row,int Col,int math[100][100]){
-    for(i=0;i<Row;i++)for(i=0;i<Row;i++){
-        for(j=0;j<Col;j++)for(j=0;j<Col;j++){
-            if(j>0){
-                printf(" ");
+void printMatrix(int Row,int Col,int math[100][100]) {
+    for(i=0;i<Row;i++){
+        for(i=0;i<Row;i++){
+            for(j=0;j<Col;j++){
+                for(j=0;j<Col;j++){
+                    if(j>0){
+                        cout << " ";
+                    }
+                    cout << "\t[" << math[i][j] << "]";
+                }
+                cout << "\n";
             }
-            printf("\t[%d]",math[i][j]);
         }
-    printf("\n");
     }
+    main();
 }
